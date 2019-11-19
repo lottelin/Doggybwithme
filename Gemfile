118c610ec304306a18fae6013843dd57ee11a2fc
Charlotte Lin <charlotte.wanchen.lin@gmail.com>
2019-11-19 11:41:49 +0100

    put dotenv gem for security into gemfiles

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 ruby '2.6.3'
 
 gem 'devise'
+gem 'dotenv-rails', groups: [:development, :test]
+# gem 'cloudinary', '~> 1.9.1'
 gem 'bootsnap', require: false
 gem 'jbuilder', '~> 2.0'
 gem 'pg', '~> 0.21'
--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,20 @@
 class DogsController < ApplicationController
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    if @dog.save
+      redirect_to @dog, notice: 'New doggy profile was created! Woof!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name :photo)
+  end
 end

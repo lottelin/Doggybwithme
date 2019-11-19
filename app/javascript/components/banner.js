58b113fae5d3eac167a8ba1c45dceab2a333882f
Charlotte Lin <charlotte.wanchen.lin@gmail.com>
2019-11-19 18:26:39 +0100

    added paw link

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["Cool Dog High 5","Just One Click Away"],
-    typeSpeed: 50,
+    typeSpeed: 40,
     loop: true
   });
 }

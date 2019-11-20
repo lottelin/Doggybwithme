8850b157b56b98b598205c2980553519b29a2711
Gordana Macura <gordanamacura@gmail.com>
2019-11-20 15:36:50 +0100

    updateduser profile page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get '/profile', to: 'pages#profile', as: :profile
+  post '/profile', to: 'pages#image_upload', as: :profile_upload
 
   resources :dogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

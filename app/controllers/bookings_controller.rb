086e3e142a8b7189951e281e7ff1b6f90150a228
Petr Zaionchkovskiy <zaionchkovskiy@gmail.com>
2019-11-20 15:40:06 +0100

    peters branch

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,17 @@
+class BookingsController < ApplicationController
+  def create
+    year = params[:booking]["start_date(1i)"].to_i
+    month = params[:booking]["start_date(2i)"].to_i
+    day = params[:booking]["start_date(3i)"].to_i
+    start_date = Date.new(year, month, day)
+
+    year = params[:booking]["end_date(1i)"].to_i
+    month = params[:booking]["end_date(2i)"].to_i
+    day = params[:booking]["end_date(3i)"].to_i
+    end_date = Date.new(year, month, day)
+
+    @booking = Booking.new(start_date: start_date, end_date: end_date, user_id: current_user.id, dog_id: params[:dog_id])
+    @booking.save!
+    redirect_to profile_path
+  end
+end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :dog
   belongs_to :user
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :dog_id, presence: true
+  validates :user_id, presence: true
 end

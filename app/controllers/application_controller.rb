709ee8ddecd6224e07f1c028d2eda127d7605d53
Petr Zaionchkovskiy <zaionchkovskiy@gmail.com>
2019-11-20 12:44:50 +0100

    implemented user functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:fullname, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:fullname, :email, :password, :current_password, :is_female, :date_of_birth) }
+  end
 end

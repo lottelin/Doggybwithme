99c9105c07aa21337de0b2b09c43750d27a46b61
Gordana Macura <gordanamacura@gmail.com>
2019-11-22 15:11:54 +0100

    added remove booking function

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   resources :dogs do
     resources :bookings, only: [:create]
   end  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bookings, only: [:destroy]
 end
